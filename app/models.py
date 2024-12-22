68e744d2bc0951d61b9d68fab0cbcad5670603aa
Татьяна Сарафанова <sara4ka0@yandex.ru>
2024-12-22 17:07:52 +0300

    Deleted comments from models

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     balance = db.Column(db.Float, default=0.0)
     commission_rate = db.Column(db.Float, default=0.0)
     webhook_url = db.Column(db.String(255), nullable=True)
-    role = db.Column(db.String(10), default='user')  # admin или user
+    role = db.Column(db.String(10), default='user')
     usdt_wallet = db.Column(db.String(255), nullable=True)
 
 
@@ -15,7 +15,7 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     commission = db.Column(db.Float, nullable=False)
-    status = db.Column(db.String(20), default='waiting')  # waiting, confirmed, canceled, expired
+    status = db.Column(db.String(20), default='waiting')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref='transactions')
