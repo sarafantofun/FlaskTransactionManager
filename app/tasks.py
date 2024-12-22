ff7e972b2621657f8ee4335e0b5bb9c1f233f08c
Татьяна Сарафанова <sara4ka0@yandex.ru>
2024-12-22 17:52:50 +0300

    Fixed bugs in tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 
+import requests
 from celery import shared_task
 
 from app import create_app
 from app.extensions import db
-from app.models import Transaction
+from app.models import Transaction, User
 
 flask_app = create_app()
 celery_app = flask_app.extensions["celery"]
@@ -13,18 +14,28 @@ celery_app = flask_app.extensions["celery"]
 @shared_task(ignore_result=False)
 def expire_transactions():
     """
-    Проверяет транзакции со статусом 'waiting' и меняет статус на 'expired',
-    если прошло более 15 минут.
+    Проверяет транзакции со статусом 'waiting', меняет статус на 'expired'
+    и отправляет вебхук с информацией о транзакции.
     """
-    now = datetime.utcnow()
-    expired_time = now - timedelta(minutes=15)
-
-    transactions = Transaction.query.filter_by(status='waiting').filter(
-        Transaction.created_at < expired_time
-    ).all()
-
-    for transaction in transactions:
-        transaction.status = 'expired'
-        db.session.commit()
-
+    app_context = flask_app.app_context()
+    with app_context:
+        now = datetime.utcnow()
+        expired_time = now - timedelta(minutes=15)
+        transactions = Transaction.query.filter_by(status='waiting').filter(
+            Transaction.created_at < expired_time
+        ).all()
+        for transaction in transactions:
+            transaction.status = 'expired'
+            db.session.commit()
+            user = User.query.get(transaction.user_id)
+            if user and user.webhook_url:
+                payload = {
+                    "transaction_id": transaction.id,
+                    "status": transaction.status
+                }
+                try:
+                    response = requests.post(user.webhook_url, json=payload)
+                    response.raise_for_status()
+                except requests.RequestException as e:
+                    print(f"Webhook failed for Transaction {transaction.id}: {e}")
     return f"{len(transactions)} транзакции(й) обновлены."
